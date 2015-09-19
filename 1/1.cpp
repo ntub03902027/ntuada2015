6bf33d5e896381211e26b9bf5825ca2ea8addb44
b03902027 <b03902027@linux6.csie.ntu.edu.tw>
2015-09-19 23:19:02 +0800

    add 1/1.cpp

---
+++ 1/1.cpp
@@ -0,0 +1,14 @@
+#include <cstdio>
+
+using namespace std;
+
+
+int main(void) {
+	int n, a, b;
+	scanf("%d", &n);
+	for (int i = 0; i < n; i++) {
+		scanf("%d%d", &a, &b);
+		printf("%d\n", (a + b));
+	}
+	return 0;
+}
