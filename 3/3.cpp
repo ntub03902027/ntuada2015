f1067e1584b1e83f8962d664f1358757656bb2eb
b03902027 <b03902027@linux6.csie.ntu.edu.tw>
2015-10-09 02:33:09 +0800

    3.cpp fifteenth try

--- 3/3.cpp
+++ 3/3.cpp
@@ -1,18 +1,19 @@
-#include <cstdio>
-#include <algorithm>
-#include <vector>
-
-using namespace std;
+#include <stdio.h>
+#include <stdlib.h>
 
 long long int c, e, p;
 int n;
 
-bool beat(int a, int b) {
+int compare(const int a, const int b) {
+	//int r = *(int *)a;
+	//int s = *(int *)b;
 	long long int ans = (c * (a - b)) % p;
 	long long int power = e;
 	long long int base = a + b;
 	if (ans < 0)
 		ans += p;
+
+
 	while (power > 0) {
 		if (power % 2 == 1) {
 			ans = (ans * base) % p;
@@ -21,48 +22,45 @@ bool beat(int a, int b) {
 		power /= 2;
 		base = (base * base) % p;
 	}
-	
-	//printf("answer of %d and %d is %d.\n", a, b, ans);
+
 	if (ans > p / 2)
-		return true;
+		return 1;
 	else
-		return false;
-
+		return 0;
 }
 
-bool compare(int a, int b) {
-	return (a > b);
-}
-
-void merge_sort(std::vector<int> &queue) {
-	if (queue.size() <= 1)
+void merge_sort(int *queue, int size) {
+	if ( size <= 1)
 		return;
-	std::vector<int> subqueue1;
-	std::vector<int> subqueue2;
-	for (int i = 0; i < queue.size() / 2; i++)
-		subqueue1.push_back(queue[i]);
-	for (int i = queue.size() / 2; i < queue.size(); i++)
-		subqueue2.push_back(queue[i]);
+	int *subqueue1;
+	subqueue1 = (int *)malloc(size / 2 * sizeof(int));
+	int *subqueue2;
+	subqueue2 = (int *)malloc((size - size / 2) * sizeof(int));
+	int size1 = size / 2;
+	int size2 = size - (size / 2);
+	for (int i = 0; i < size / 2; i++)
+		subqueue1[i] = queue[i];
+	for (int i = size / 2; i < size; i++)
+		subqueue2[i - (size / 2)] = queue[i];
 
-	//queue.clear();
-	merge_sort(subqueue1);
-	merge_sort(subqueue2);
-	
-	if (beat(subqueue1[subqueue1.size() - 1], subqueue2[0])) {
-		for (int i = 0; i < subqueue1.size(); i++)
+	merge_sort(subqueue1, size1);
+	merge_sort(subqueue2, size2);
+
+	if (compare(subqueue1[size1 - 1], subqueue2[0])) {
+		for (int i = 0; i < size1; i++)
 			queue[i] = subqueue1[i];
-		for (int i = subqueue1.size(); i < queue.size(); i++)
-			queue[i] = subqueue2[i - subqueue1.size()];
+		for (int i = size1; i < size; i++)
+			queue[i] = subqueue2[i - size1];
 	}
-	else if (beat(subqueue2[subqueue2.size() - 1], subqueue1[0])) {
-		for (int i = 0; i < subqueue2.size(); i++)
+	else if (compare(subqueue2[size2 - 1], subqueue1[0])) {
+		for (int i = 0; i < size2; i++)
 			queue[i] = subqueue2[i];
-		for (int i = subqueue2.size(); i < queue.size(); i++)
-			queue[i] = subqueue1[i - subqueue2.size()];
+		for (int i = size2; i < size; i++)
+			queue[i] = subqueue1[i - size2];
 	}
 	else {
-		for (int i = 0, it1 = 0, it2 = 0; i < subqueue1.size() + subqueue2.size(); i++) {
-			if ( it2 >= subqueue2.size() || (it1 < subqueue1.size() && beat(subqueue1[it1], subqueue2[it2]))) {
+		for (int i = 0, it1 = 0, it2 = 0; i < size; i++) {
+			if ( it2 >= size2 || (it1 < size1 && compare(subqueue1[it1], subqueue2[it2]))) {
 				//queue.push_back(subqueue1[it1]);
 				queue[i] = subqueue1[it1];
 				it1++;
@@ -74,50 +72,32 @@ void merge_sort(std::vector<int> &queue) {
 			}
 		}
 	}
-	subqueue1.clear();
-	subqueue2.clear();
+	free(subqueue1);
+	free(subqueue2);
+	
+	/*printf("///\n");
+	for (int i = 0; i < size; i++)
+			printf("%d ", queue[i]);
+		printf("\n");*/
 }
-/*
-void examine_ans(std::vector<int> &queue, int &c, long long int &e, int &p) {
-	int exarr[queue.size() + 1];
-	for (int i = 0; i <= queue.size(); i++)
-		exarr[i] = 0;
-	for (int i = 0; i < queue.size() - 1; i++) {
-		if (!beat(queue[i], queue[i + 1], c, e, p) || exarr[queue[i]] != 0) {
-			printf("wrong\n");
-			if (exarr[queue[i]] != 0)
-				printf("%d has more than one in %d. Why?\n", queue[i], i);
-			return;
-		}
-		exarr[queue[i]]++;
-	}
-	if (exarr[queue[queue.size() - 1]] != 0) {
-		printf("wrong\n");
-		printf("%d has more than one. Why?\n", queue.size());
-		return;
-	}
-
-	printf("correct\n"); 
-	return;
-}*/
 
 int main(void) {
 	int T;
 	scanf("%d", &T);
-	std::vector<int> queue;
+	int *queue;
+	queue = (int *)malloc(200000* sizeof(int));
 	while (T--) {
-		scanf("%d%d%lld%d", &n, &c, &e, &p);
-
-		e = e % (p - 1); /*fermat theorem*/
+		scanf("%d%lld%lld%lld", &n, &c, &e, &p);
+		e = e % (p - 1);
 		for (int i = 0; i < n; i++)
-			queue.push_back(i + 1);
-		merge_sort(queue);
+			queue[i] = i + 1;
+		merge_sort(queue, n);
+
+		//qsort(queue, n, sizeof(int), compare);
 
 		for (int i = 0; i < n; i++)
 			printf("%d ", queue[i]);
 		printf("\n");
-
-		queue.clear();
 	}
-	return 0;
+
 }
